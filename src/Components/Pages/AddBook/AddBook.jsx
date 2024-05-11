8345429ae21d9e411eaa33f35eadafc8a8cb0e64
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-05-11 11:44:16 +0600

    tab added

--- src/Components/Pages/AddBook/AddBook.jsx
+++ src/Components/Pages/AddBook/AddBook.jsx
@@ -1,62 +1,73 @@
-
+import { useForm } from "react-hook-form"
+import useProvider from "../../Provider/useProvider";
 const AddBook = () => {
+    const { users } = useProvider()
+    const { register, handleSubmit, watch, formState: { errors }, } = useForm()
+
+    const onSubmit = (data) => {
+        const email = users.email
+        const newdata = { ...data, email }
+
+        console.log(newdata)
+    }
     return (
         <div className='container mx-auto bg-[#FEF3F0] md:my-20 rounded-xl drop-shadow-md'>
             <h2 className='text-3xl font-platypi font-semibold text-center pt-10  text-[#C20E1A]'>Add Your <span className='text-black'> Book</span></h2>
             <p className='text-center font-poppins text-[#B09CA9] my-5 px-5'> You present Your self with book, You present Your self with book, You present Your self with book,  </p>
-            <form className="card-body md:grid grid-cols-2 gap-x-10 px-20">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body md:grid grid-cols-2 gap-x-10 px-20">
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-platypi">Book Name</span>
                     </label>
-                    <input type="text" placeholder="Book Name" className="input input-bordered bg-[#fff]" required />
+                    <input type="text" {...register("bookName", { required: true })} placeholder="Book Name" className="input input-bordered bg-[#fff]" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-platypi">Quantity</span>
                     </label>
-                    <input type="number" placeholder="1" className="input input-bordered  bg-[#fff]" required />
+                    <input type="number" {...register("quantity", { required: true })} placeholder="1" className="input input-bordered  bg-[#fff]" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-platypi">Author Name</span>
                     </label>
-                    <input type="text" placeholder="Author Name" className="input input-bordered bg-[#fff]" required />
+                    <input type="text" {...register("authorName", { required: true })} placeholder="Author Name" className="input input-bordered bg-[#fff]" required />
                 </div>
                 <div className="form-control ">
                     <label className="label">
                         <span className="label-text text-lg font-platypi">Category</span>
                     </label>
-                    <select name="category" id="" className='p-3 rounded-lg '>
+                    <select name="category" {...register("category", { required: true })} id="" className='p-3 rounded-lg '>
                         <option value="romantic">Romantic</option>
-                        <option value="romantic1">Romantic1</option>
-                        <option value="romantic3">Romantic3</option>
+                        <option value="history">History</option>
+                        <option value="religion">Religion</option>
+                        <option value="kids">Kids</option>
                     </select>
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-platypi">Rating</span>
                     </label>
-                    <input type="number" min={1} max={5} placeholder="Author Name" className="input input-bordered bg-[#fff]" required />
+                    <input type="number" {...register("rating", { required: true })} min={1} max={5} placeholder="Author Name" className="input input-bordered bg-[#fff]" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-platypi">Photo</span>
                     </label>
-                    <input type="url" placeholder=" Img URL" className="input input-bordered bg-[#fff]" required />
+                    <input type="url" {...register("photo", { required: true })} placeholder=" Img URL" className="input input-bordered bg-[#fff]" required />
                 </div>
 
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-platypi">Description</span>
                     </label>
-                    <textarea className="textarea textarea-primary" placeholder="Bio"></textarea>
+                    <textarea {...register("description", { required: true })} className="textarea textarea-primary" placeholder="Bio"></textarea>
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-platypi">About of Book</span>
                     </label>
-                    <textarea className="textarea textarea-primary" placeholder="Bio"></textarea>
+                    <textarea {...register("about", { required: true })} className="textarea textarea-primary" placeholder="Bio"></textarea>
                 </div>
 
                 <div className="form-control mt-6 md:col-span-2 ">
--- src/Components/Pages/AllBook/Books.jsx
+++ src/Components/Pages/AllBook/Books.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import BookCard from './BookCard';
 
+
 const Books = () => {
     const arr = [1, 2, 3, 4, 5, 6]
     return (
-        <div className='container mx-auto grid grid-cols-3 gap-5 my-10'>
-            {arr.map(ar => <BookCard key={ar}></BookCard>)}
+        <div className='container mx-auto my-10 '>
+            <div role="tablist" className="tabs tabs-lifted  ">
+                <input type="radio" name="my_tabs_1" role="tab" className="tab text-xl font-platypi" aria-label="Romantic" />
+                <div role="tabpanel" className="tab-content p-10">
+
+                    <div className=' grid grid-cols-3 gap-5 '>
+                        {arr.map(ar => <BookCard key={ar}></BookCard>)}
+                    </div>
+                </div>
+
+                <input type="radio" name="my_tabs_1" role="tab" className="tab text-xl font-platypi" aria-label="History" checked />
+                <div role="tabpanel" className="tab-content p-10">Tab content 2</div>
+
+                <input type="radio" name="my_tabs_1" role="tab" className="tab text-xl font-platypi" aria-label="Religion" />
+                <div role="tabpanel" className="tab-content p-10">Tab content 3</div>
+                <input type="radio" name="my_tabs_1" role="tab" className="tab text-xl font-platypi" aria-label="Kids" />
+                <div role="tabpanel" className="tab-content p-10">Tab content 4</div>
+            </div>
+
         </div>
+
+
+
     );
 };
 
